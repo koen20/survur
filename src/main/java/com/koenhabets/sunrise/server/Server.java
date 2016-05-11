c0206ea6115d67c2eabb8459b568d7b35769bbff
Koen Habets <koenhabets1@gmail.com>
2016-05-11 16:22:49 +0200

    Initial commit

---
+++ src/main/java/com/koenhabets/sunrise/server/Server.java
@@ -0,0 +1,21 @@
+package com.koenhabets.sunrise.server;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * Created by koenh on 8-5-2016.
+ */
+public class Server {
+
+    public static void main(String args[]) throws IOException {
+        int port = 9999;
+        System.out.println("Starting server on port: " + port);
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/sunset-sunrise", new SunSetHandler());
+        server.start();
+        System.out.println("Server started.");
+    }
+}
