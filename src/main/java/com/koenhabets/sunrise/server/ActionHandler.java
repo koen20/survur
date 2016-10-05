c03f4064c1ead1c119303807e93c5b997e8837c2
koenkoe <koenhabets1@gmail.com>
2016-10-05 22:10:33 +0200

    Added alarms

--- src/main/java/com/koenhabets/sunrise/server/ActionHandler.java
+++ src/main/java/com/koenhabets/sunrise/server/ActionHandler.java
@@ -5,10 +5,12 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Calendar;
 import java.util.Objects;
 
 public class ActionHandler implements HttpHandler {
     String response = "sent";
+    int code = 200;
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         String parm = httpExchange.getRequestURI().getQuery();
@@ -21,8 +23,29 @@ public class ActionHandler implements HttpHandler {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        } else if(Objects.equals(action, "Prep-Sleep")){
+            Calendar calendar = Calendar.getInstance();
+            int day = calendar.get(Calendar.DAY_OF_WEEK);
+            if(day == 4){
+                try {
+                    //// TODO: 10/5/2016 Controle of ik het eerste uur vrij heb 
+                    CalendarHandler.setAlarm("08","15");
+                    VoiceHandler.sendPost("Welterusten. Je heb het eerste uur vrij. Het alarm gaat om 08:15", "voice");
+                } catch (Exception e) {
+                    code = 500;
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    CalendarHandler.setAlarm("07","25");
+                    VoiceHandler.sendPost("Welterusten. Het alarm gaat om 07:25", "voice");
+                } catch (Exception e) {
+                    code = 500;
+                    e.printStackTrace();
+                }
+            }
         }
-        httpExchange.sendResponseHeaders(200, response.length());
+        httpExchange.sendResponseHeaders(code, response.length());
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
--- src/main/java/com/koenhabets/sunrise/server/CalendarHandler.java
+++ src/main/java/com/koenhabets/sunrise/server/CalendarHandler.java
@@ -3,8 +3,10 @@ package com.koenhabets.sunrise.server;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Objects;
 
 public class CalendarHandler implements HttpHandler {
@@ -24,5 +26,37 @@ public class CalendarHandler implements HttpHandler {
     }
     static String getResponse(){
         return response;
+    }
+
+    public static String setAlarm(String hour, String minute) throws Exception {
+        String url = "https://autoremotejoaomgcd.appspot.com/sendmessage?key=" + KeyHolder.getArKey2() + "&message=" + "alarm" + ";" + hour+";"+minute;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        //con.setRequestProperty("User-Agent", USER_AGENT);
+        //con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
     }
 }
