247523766371d9c7abedc35e8f13e27d607a8af5
koenkoe <koenhabets1@gmail.com>
2016-10-29 16:37:19 +0200

    Disable lcd backlight if away

--- src/main/java/com/koenhabets/sunrise/server/ActionHandler.java
+++ src/main/java/com/koenhabets/sunrise/server/ActionHandler.java
@@ -11,38 +11,48 @@ public class ActionHandler implements HttpHandler {
     String response = "sent";
     private int code = 200;
     static boolean sleeping = false;
+    static boolean inside = true;
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
         String parm = httpExchange.getRequestURI().getQuery();
         String[] parts = parm.split("=");
         String action = parts[1];
-        if (Objects.equals(action, "Wake-up")){
+        if (Objects.equals(action, "Wake-up")) {
             sleeping = false;
             try {
                 int temp = WeatherHandler.getTemp();
-                VoiceHandler.sendPost("Goedemorgen Koen. Het is "+ temp + " graden buiten. Je volgende afspraak is: "+ CalendarHandler.getResponse(), "voice");
+                VoiceHandler.sendPost("Goedemorgen Koen. Het is " + temp + " graden buiten. Je volgende afspraak is: " + CalendarHandler.getResponse(), "voice");
                 //VoiceHandler.sendPost("scholica","app");
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } else if(Objects.equals(action, "Prep-Sleep")){
-                LcdHandler.disableBacklight();
-                sleeping = true;
-                try {
-                    //// TODO: 10/5/2016 Controle of ik het eerste uur vrij heb
-                    VoiceHandler.sendPost("Welterusten. Wil je morgen douchen?;Prep-Sleep", "voice");
-                    VoiceHandler.sendPost("", "response");
-                } catch (Exception e) {
-                    code = 500;
-                    e.printStackTrace();
-                }
-                try {
-                    VoiceHandler.sendPost("Welterusten. Wil je morgen douchen?;Prep-Sleep", "voice");
-                    VoiceHandler.sendPost("", "response");
-                } catch (Exception e) {
-                    code = 500;
-                    e.printStackTrace();
-                }
+        } else if (Objects.equals(action, "Prep-Sleep")) {
+            LcdHandler.disableBacklight();
+            sleeping = true;
+            try {
+                //// TODO: 10/5/2016 Controle of ik het eerste uur vrij heb
+                VoiceHandler.sendPost("Welterusten. Wil je morgen douchen?;Prep-Sleep", "voice");
+                VoiceHandler.sendPost("", "response");
+            } catch (Exception e) {
+                code = 500;
+                e.printStackTrace();
+            }
+            try {
+                VoiceHandler.sendPost("Welterusten. Wil je morgen douchen?;Prep-Sleep", "voice");
+                VoiceHandler.sendPost("", "response");
+            } catch (Exception e) {
+                code = 500;
+                e.printStackTrace();
+            }
+        } else if (Objects.equals(action, "Enter")) {
+            if(!sleeping) {
+                inside = true;
+            }
+        } else if (Objects.equals(action, "Leave")) {
+            if(!sleeping) {
+                inside = false;
+            }
         }
         httpExchange.sendResponseHeaders(code, response.length());
         OutputStream os = httpExchange.getResponseBody();
