c232c49b0bd15fa19a1b0c23c30796297b745061
koenkoe <koenhabets1@gmail.com>
2016-10-29 15:18:46 +0200

    Display switches between outside and inside temp

--- src/main/java/com/koenhabets/sunrise/server/Timer.java
+++ src/main/java/com/koenhabets/sunrise/server/Timer.java
@@ -5,21 +5,30 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class timer extends TimerTask {
+    int d = 0;
 
     @Override
     public void run() {
-        if(!ActionHandler.sleeping) {
+        if (!ActionHandler.sleeping) {
             Calendar calendar = Calendar.getInstance();
             int hour = calendar.get(Calendar.HOUR_OF_DAY);
             int minute = calendar.get(Calendar.MINUTE);
             String temp = TemperatureHandler.getTemp();
-            LcdHandler.printLcd(hour + ":" + minute,"Binnen: " + temp);
+            int outsideTemp = WeatherHandler.getTemp();
+            if (d == 0) {
+                LcdHandler.printLcd(hour + ":" + minute, "Binnen:" + temp);
+                d = 1;
+            } else {
+                LcdHandler.printLcd(hour + ":" + minute, "Buiten:" + outsideTemp);
+                d = 0;
+            }
+
         }
     }
 
-    public static void main(){
+    public static void main() {
         TimerTask timerTask = new timer();
         Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(timerTask, 0, 30*1000);
+        timer.scheduleAtFixedRate(timerTask, 0, 30 * 1000);
     }
 }
