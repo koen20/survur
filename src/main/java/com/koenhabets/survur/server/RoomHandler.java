8c874d6cb227c1d0f830a0f566b186ccd3b2ae29
Koen Habets <koenhabets1@gmail.com>
2017-02-13 13:43:47 +0100

    Improved inside detection

--- src/main/java/com/koenhabets/survur/server/RoomHandler.java
+++ src/main/java/com/koenhabets/survur/server/RoomHandler.java
@@ -6,10 +6,17 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class RoomHandler implements HttpHandler {
     static boolean insideRoom = false;
     String response;
+
+    public RoomHandler() {
+        Timer updateTimer = new Timer();
+        updateTimer.scheduleAtFixedRate(new CheckWifi(), 0, 3 * 60 * 1000);
+    }
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
@@ -27,5 +34,20 @@ public class RoomHandler implements HttpHandler {
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
+    }
+
+    private class CheckWifi extends TimerTask {
+
+
+        @Override
+        public void run() {
+            ExecuteShellCommand com = new ExecuteShellCommand();
+            String d = com.executeCommand("bash /home/pi/scripts/wifiScan");
+            if(Objects.equals(d, "")){
+                ActionHandler.inside = false;
+            } else {
+                ActionHandler.inside = true;
+            }
+        }
     }
 }
