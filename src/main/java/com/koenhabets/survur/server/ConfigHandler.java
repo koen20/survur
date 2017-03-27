8909db74c1064f72ddb4fb87e9a9164935b74be9
Koen Habets <koenhabets1@gmail.com>
2017-03-27 13:22:16 +0200

    Added config save

--- src/main/java/com/koenhabets/survur/server/ConfigHandler.java
+++ src/main/java/com/koenhabets/survur/server/ConfigHandler.java
@@ -1,8 +1,13 @@
 package com.koenhabets.survur.server;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 
@@ -10,7 +15,15 @@ public class ConfigHandler {
     static boolean alarmEnabled = true;
     static boolean motionEnabled = true;
 
-    public String setConfig(Request request, Response response) {
+    public ConfigHandler() {
+        try {
+            readSavedConfig();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String setConfig(Request request, Response response) throws IOException {
         String parm = request.queryParams("config");
         String parm2 = request.queryParams("status");
         if (Objects.equals(parm, "alarm")) {
@@ -19,13 +32,31 @@ public class ConfigHandler {
             } else {
                 alarmEnabled = false;
             }
+            saveConfig();
         } else if (Objects.equals(parm, "motion")) {
             if (Objects.equals(parm2, "true")) {
                 motionEnabled = true;
             } else {
                 motionEnabled = false;
             }
+            saveConfig();
         }
         return "";
+    }
+
+    private void readSavedConfig() throws IOException {
+        File file = new File("config.txt");
+        String result = Files.toString(file, Charsets.UTF_8);
+        JSONObject jo = new JSONObject(result);
+        alarmEnabled = jo.getBoolean("alarm");
+        motionEnabled = jo.getBoolean("motion");
+    }
+
+    private void saveConfig() throws IOException {
+        JSONObject jo = new JSONObject();
+        jo.put("alarm", alarmEnabled);
+        jo.put("motion", motionEnabled);
+        File file = new File("config.txt");
+        Files.write(jo.toString(), file, Charsets.UTF_8);
     }
 }
